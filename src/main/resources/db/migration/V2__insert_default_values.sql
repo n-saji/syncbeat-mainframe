50279dc0f303ce3a445f49fa8874468c4c2803f0
Nikhil Saji <nikhilsaji200@gmail.com>
2026-07-25 15:55:08 -0400

    update configuration and database scripts; add actuator dependency and default user

---
+++ src/main/resources/db/migration/V2__insert_default_values.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(id, f_name, l_name, email, password, is_admin, created_at, updated_at) VALUES
+('00000000-0000-0000-0000-000000000001', 'Admin', 'User', 'admin@example.com', '$2a$12$2dWwcr4.ApSAJgjnh2fek.Q1a5w9CEs3omyeI1e8Uzo9zmKYctO0a', true, NOW(),
+ NOW());
