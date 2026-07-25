de877300f43e5a0f8b56bc6013f44b7d5a9adde6
Nikhil Saji <nikhilsaji200@gmail.com>
2026-07-25 13:02:35 -0400

    initial commit

---
+++ src/main/java/com/syncbeat/mainframe/syncbeatmainframe/SyncbeatMainframeApplication.java
@@ -0,0 +1,13 @@
+package com.syncbeat.mainframe.syncbeatmainframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncbeatMainframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncbeatMainframeApplication.class, args);
+	}
+
+}
