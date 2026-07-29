4455a38369fc21ee7b84a423b7caa75e3890edef
Nikhil Saji <nikhilsaji200@gmail.com>
2026-07-29 16:53:20 -0400

    Rooms done, need to implement additional business logic

---
+++ src/main/java/com/syncbeat/mainframe/syncbeatmainframe/dto/RoomCreationRequestDto.java
@@ -0,0 +1,19 @@
+package com.syncbeat.mainframe.syncbeatmainframe.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomCreationRequestDto {
+	private String name;
+	@JsonProperty("is_public")
+	private boolean isPublic;
+}
