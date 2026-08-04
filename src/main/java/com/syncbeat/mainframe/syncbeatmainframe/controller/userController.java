c4cc25580bcdc3a142f9333c63ae2975a1bb38e9
Nikhil Saji <nikhilsaji200@gmail.com>
2026-08-04 11:45:43 -0400

    removed private keys

--- src/main/java/com/syncbeat/mainframe/syncbeatmainframe/controller/userController.java
+++ src/main/java/com/syncbeat/mainframe/syncbeatmainframe/controller/userController.java
@@ -7,10 +7,8 @@ import com.syncbeat.mainframe.syncbeatmainframe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
